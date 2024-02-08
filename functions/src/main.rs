b790cb938e7e0d73f4d5179c1229eea4407d5c0d
David Bliss <vivid.map6767@davidbliss.me>
2024-02-08 20:39:54 +0000

    plus_one

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,7 +2,10 @@ fn main() {
     print_labeled_measurement(5, 'h');
 
     let x = five();
-    println!("{x}");
+    println!("The value of x is: {x}");
+
+    let x = plus_one(x);
+    println!("The value of x is: {x}");
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
@@ -11,4 +14,8 @@ fn print_labeled_measurement(value: i32, unit_label: char) {
 
 fn five() -> i32 {
     5
+}
+
+fn plus_one(value: i32) -> i32 {
+    value + 1
 }
