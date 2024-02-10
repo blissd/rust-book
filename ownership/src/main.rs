7c97987932cbc241240ea514d364d8d6f0f500cb
David Bliss <vivid.map6767@davidbliss.me>
2024-02-10 21:44:48 +0000

    mutable reference

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,10 +1,8 @@
 fn main() {
-    let s1 = String::from("hello");
-    let len = calculate_length(&s1);
-
-    println!("The length of '{}' is '{}'", s1, len);
+    let mut s = String::from("hello");
+    change(&mut s);
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn change(s: &mut String)  {
+    s.push_str(", world");
 }
