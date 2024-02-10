b7c41c75f6830c4c4228b7cd52f9d793d3f6172f
David Bliss <vivid.map6767@davidbliss.me>
2024-02-10 07:16:21 +0000

    fibonacci sequence - recursive

---
+++ fib/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    for n in (0..20) {
+        println!("fib({n}) = {}", fib(n));
+    }
+}
+
+fn fib(n: i32) -> i32 {
+    if n <= 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fib(n - 2) + fib(n - 1)
+    }
+}
+
