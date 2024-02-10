e36511ae727259004666c036dfeaff427f8a8f44
David Bliss <vivid.map6767@davidbliss.me>
2024-02-10 11:51:35 +0000

    ownership and functions

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,15 @@
 fn main() {
-    let s1 = String::from("hello");
-    let s2 = s1.clone();
+    let s = String::from("hello");
+    take_ownership(s);
 
-    println!("s1 = {}, s2 = {}", s1, s2);
+    let x = 5;
+    makes_copy(x);
+}
+
+fn take_ownership(s: String) {
+    println!("{}", s);
+}
+
+fn makes_copy(x: i32) {
+    println!("{}", x);
 }
