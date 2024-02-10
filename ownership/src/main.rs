303842850b0a438ec3f1c96a52a0c7735b09f3aa
David Bliss <vivid.map6767@davidbliss.me>
2024-02-10 14:33:05 +0000

    return ownership

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,16 +1,11 @@
 fn main() {
-    let s1 = give_ownership();
+    let s1 = String::from("hello");
+    let (s2, len) = calculate_length(s1);
 
-    let s2 = String::from("hello");
-
-    let s3 = takes_and_gives_back(s2);
+    println!("The length of '{}' is '{}'", s2, len);
 }
 
-fn give_ownership() -> String {
-    let some_string = String::from("yours");
-    some_string
-}
-
-fn takes_and_gives_back(s: String) -> String {
-    s
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)
 }
