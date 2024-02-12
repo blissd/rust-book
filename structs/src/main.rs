d120a5677f76d7129b8a6ccbaf30ba8c705af1ef
David Bliss <vivid.map6767@davidbliss.me>
2024-02-12 18:18:45 +0000

    returning a struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -6,10 +6,21 @@ struct User {
 }
 
 fn main() {
-    let user1 = User {
+    let mut user1 = User {
         active: true,
         username: String::from("someusername123"),
         email: String::from("someone@example.com"),
         sign_in_count: 1,
     };
+
+    user1.email = String::from("foo@example.com");
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
 }
