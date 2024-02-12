0e29d510e167b306af449c0654da1550c445d614
David Bliss <vivid.map6767@davidbliss.me>
2024-02-12 18:27:26 +0000

    unit struct

--- tuple_struct/src/main.rs
+++ tuple_struct/src/main.rs
@@ -1,7 +1,10 @@
 struct Colour(i32, i32, i32);
 struct Point(i32, i32, i32);
 
+struct AlwaysEqual;
+
 fn main() {
     let black = Colour(0, 0, 0);
     let origin = Point(0, 0, 0);
+    let subject = AlwaysEqual;
 }
