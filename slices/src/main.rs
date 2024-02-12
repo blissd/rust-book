0a1476f63c265b99561227ca20763f9b8a854c47
David Bliss <vivid.map6767@davidbliss.me>
2024-02-12 18:09:32 +0000

    more flexible signature

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -4,7 +4,7 @@ fn main() {
     println!("{} first word is {}", s, w);
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
