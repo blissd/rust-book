fcf99fee0495cb8afa13390ed6bbd0a9bc368ff1
David Bliss <vivid.map6767@davidbliss.me>
2024-02-14 20:16:23 +0000

    rectangle - impl

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -3,22 +3,23 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }    
+}
+
 fn main() {
-    let scale = 2;
     
     let rect1 = Rectangle {
-        width: dbg!(30 * scale),
+        width: 30,
         height: 50,
     };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-
-    dbg!(rect1);
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
