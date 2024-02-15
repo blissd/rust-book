4253200632653fdb150c9684af6923e465769ba6
David Bliss <vivid.map6767@davidbliss.me>
2024-02-15 13:11:45 +0000

    enums with functions

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -3,8 +3,24 @@ enum IpAddr {
     V6(String),
 }
 
+enum Message {
+    Quit,
+    Move{x: i32, y: i32},
+    Write(String),
+    ChangeColour(i32, i32, i32),
+
+}
+
+impl Message {
+    fn call(&self) {
+        println!("message called")
+    }
+}
 fn main() {
     let home = IpAddr::V4(127, 0, 0, 1);
     let loopback = IpAddr::V6(String::from("::1"));
+
+    let msg = Message::Write(String::from("Foo"));
+    msg.call();
 
 }
