e13bb2a8a53b27f22de22ce4dde062d52686633e
David Bliss <vivid.map6767@davidbliss.me>
2024-02-23 11:39:02 +0000

    largest char

--- generic_data_types/src/main.rs
+++ generic_data_types/src/main.rs
@@ -1,4 +1,15 @@
-fn largest(list: &Vec<i32>) -> &i32 {
+fn largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for number in list {
+        if number > largest {
+            largest = number;
+        }
+    }
+    largest
+}
+
+fn largest_char(list: &[char]) -> &char {
     let mut largest = &list[0];
 
     for number in list {
@@ -11,6 +22,12 @@ fn largest(list: &Vec<i32>) -> &i32 {
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
-    let largest = largest(&number_list);
-    println!("The largest number is {}", largest);
+
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
 }
