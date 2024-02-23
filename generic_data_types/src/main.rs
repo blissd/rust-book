cb90139deadfe6f8564eb126df2a119b58213757
David Bliss <vivid.map6767@davidbliss.me>
2024-02-23 13:10:27 +0000

    Add PartialOrd

--- generic_data_types/src/main.rs
+++ generic_data_types/src/main.rs
@@ -1,20 +1,9 @@
-fn largest_i32(list: &[i32]) -> &i32 {
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
     let mut largest = &list[0];
 
-    for number in list {
-        if number > largest {
-            largest = number;
-        }
-    }
-    largest
-}
-
-fn largest_char(list: &[char]) -> &char {
-    let mut largest = &list[0];
-
-    for number in list {
-        if number > largest {
-            largest = number;
+    for item in list {
+        if item > largest {
+            largest = item;
         }
     }
     largest
@@ -23,11 +12,11 @@ fn largest_char(list: &[char]) -> &char {
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
+    let result = largest(&number_list);
     println!("The largest number is {}", result);
 
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
 }
