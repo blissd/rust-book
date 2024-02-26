a700b1e7be19f0481143bbf4566dc6c49e657a6a
David Bliss <vivid.map6767@davidbliss.me>
2024-02-26 17:36:37 +0000

    minigrep - add failing case_insenstive test

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -42,14 +42,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.
-        ";
+Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "RuSt";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(vec!["Rust", "Trust me."], search(query, contents));
     }
 }
