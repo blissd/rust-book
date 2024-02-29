06e6cc8a0fcae9b1e3840d5ef28491f12a226260
David Bliss <vivid.map6767@davidbliss.me>
2024-02-29 18:14:05 +0000

    counters

--- boxes/src/main.rs
+++ boxes/src/main.rs
@@ -1,62 +1,20 @@
-use std::ops::Deref;
+use std::rc::Rc;
 
 enum List {
-    Cons(i32, Box<List>),
+    Cons(i32, Rc<List>),
     Nil,
 }
 
 use crate::List::{Cons, Nil};
 
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
-}
-
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-fn hello(name: &str) {
-    println!("Hello, {name}!");
-}
-
-struct CustomSmartPointer {
-    data: String,
-}
-
-impl Drop for CustomSmartPointer {
-    fn drop(&mut self) {
-        println!("Dropping CustomSmartPointer with data '{}'!", self.data);
-    }
-}
-
 fn main() {
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-
-    let x = 5;
-    let y = MyBox::new(x);
-
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
-
-    let m = MyBox::new(String::from("Johny"));
-    hello(&m);
-
-    let c = CustomSmartPointer {
-        data: String::from("my stuff"),
-    };
-
-    let d = CustomSmartPointer {
-        data: String::from("other stuff"),
-    };
-
-    println!("CustomSmartPointers created");
-    drop(c); // early drop
+    let a = Rc::new(Cons(1, Rc::new(Cons(2, Rc::new(Cons(3, Rc::new(Nil)))))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
